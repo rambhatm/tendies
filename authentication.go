30b6af34d6d486bb2779aa4c0b9827eb1d2168df
ram <rammulky@gmail.com>
2019-09-24 16:59:57 -0400

    pass bncrypt

--- authentication.go
+++ authentication.go
@@ -1,7 +1,17 @@
 package main
 
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
 type AuthData struct {
 	Username     string
-	PasswordHash string //bcrypt hash of password
+	IsTrial      bool   //true : no password, only username. trial users like discord
+	PasswordHash []byte //bcrypt hash of password
 }
 
+func (a *AuthData) HashPassword(plaintextPassword string) {
+	a.PasswordHash, _ = bcrypt.GenerateFromPassword([]byte(plaintextPassword), bcrypt.MinCost)
+}
+
+func (a AuthData) IsMatch(plai)
